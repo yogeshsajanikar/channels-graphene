07f7d57f88aa62d06cec86bf1ac4b4859d0ee878
Yogesh Sajanikar <yogesh.sajanikar@yahoo.com>
2018-04-26 11:56:20 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+version = '0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+setup(
+    name='channels-graphene',
+    version=version,
+    description="'GraphQL support for channels consumers'",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+        # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers  # noqa: E501
+    ],
+    keywords='api graphql graphene django channels',
+    author='Yogesh Sajanikar',
+    author_email='yogesh_sajanikar@yahoo.com',
+    url='',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': ['channels-graphene=channelsgraphene:main']
+    })
